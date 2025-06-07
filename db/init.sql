80388d7697bc39470d83bfc3609a7ab28f4ad860
0herm <0herm@protonmail.com>
2025-06-07 15:13:47 +0200

    moved db to simpler design (one to one)

--- db/init.sql
+++ db/init.sql
@@ -1,19 +1,15 @@
--- Lists
+-- Lists table
 CREATE TABLE IF NOT EXISTS Lists (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
--- Media
+-- Media table
 CREATE TABLE IF NOT EXISTS Media (
     id SERIAL PRIMARY KEY,
     tmdb_id INTEGER NOT NULL,
-    type TEXT CHECK (type IN ('movie', 'show'))
-);
-
--- ListMedia
-CREATE TABLE IF NOT EXISTS ListMedia (
-    list_id INTEGER REFERENCES Lists(id) ON DELETE CASCADE,
-    media_id INTEGER REFERENCES Media(id) ON DELETE CASCADE,
-    PRIMARY KEY (list_id, media_id)
+    type TEXT CHECK (type IN ('movie', 'show')),
+    added_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    list_id INTEGER REFERENCES Lists(id) ON DELETE CASCADE
 );
